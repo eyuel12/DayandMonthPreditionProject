a5fbad543e036ca468db32e90134f25d3085765b
eyuel <eyuel.negash.2022@student.lhschools.org>
2019-04-05 14:20:25 -0400

    added metodes

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,24 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+      String h = ;
+
+
+    }
+
+
+   public static void main() {
+
+
+
+
+    }
+    public static void main() {
+}
+
+  public static void
