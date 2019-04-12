660c6fd498513b8b20958f7abd35faa2bd46a1de
eyuel <eyuel.negash.2022@student.lhschools.org>
2019-04-12 14:20:19 -0400

    initialized the day

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,20 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-      String h = ;
-
-
-    }
-
-
-   public static void main() {
 
 
 
 
     }
-    public static void main() {
+
+
+   public static int formofbirthmonth() {
+     Scanner birth =new Scanner(System.in);
+
+    }
+    public static int dayoftheweek (){
+        int sunday,moneday,thuseday,wednesday,thursday,day6,day7 ;
+        sunday =1;
+        moneday=1;
+        thuseday=2;
+        wednesday=3;
+        thursday=4;
+        day6=5;
+        day7=6;
+ }
+
+  public static int poenoftheday (){
+    }
 }
-
-  public static void
